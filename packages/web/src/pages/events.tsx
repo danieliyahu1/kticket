9480c1dd74707d0de2c4d010fda418356032e945
danieliyahu1 <danieliyahu1@gmail.com>
2026-08-11 13:42:24 +0300

    fix: load tickets and created events after connecting on-tab

--- packages/web/src/pages/events.tsx
+++ packages/web/src/pages/events.tsx
@@ -169,7 +169,7 @@ export default function EventsPage() {
     return () => {
       cancelled = true;
     };
-  }, [filterOrganizer, segment]);
+  }, [filterOrganizer, segment, loadTickets]);
 
   const visible = events;
 
